236221b2437875d95edb469897c86d794b3eefb0
bharani2310 <124676061+bharani2310@users.noreply.github.com>
2025-02-10 10:09:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,15 +8,15 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { AuthContextProvider } from "./context/AuthContext";
 
-import { BrowserRouter } from "react-router-dom"; 
+import { HashRouter } from "react-router-dom"; 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-    <BrowserRouter>
+    <HashRouter>
        <App />
-    </BrowserRouter>
+    </HashRouter>
     </AuthContextProvider>
   </React.StrictMode>
 );
