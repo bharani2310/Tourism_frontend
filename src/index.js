a73c6d00f11de53fc93fb941a165673124f1a953
bharani2310 <124676061+bharani2310@users.noreply.github.com>
2025-02-11 21:47:10 +0530

    url changes

--- src/index.js
+++ src/index.js
@@ -6,26 +6,15 @@ import "remixicon/fonts/remixicon.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { AuthContextProvider } from "./context/AuthContext";
-<<<<<<< HEAD
-
-import { HashRouter } from "react-router-dom"; 
-=======
 import { HashRouter } from "react-router-dom";  // Import HashRouter
->>>>>>> 7777d8a (url changes)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-<<<<<<< HEAD
-    <HashRouter>
-       <App />
-    </HashRouter>
-=======
       <HashRouter>
         <App />
       </HashRouter>
->>>>>>> 7777d8a (url changes)
     </AuthContextProvider>
   </React.StrictMode>
 );
